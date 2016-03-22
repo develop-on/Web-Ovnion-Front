08312fa18f99dae23b499bea4bd7c3763f3b9ebb
Marcos Flores <sherix88@gmail.com>
2016-03-22 14:32:23 -0300

    formulario

---
+++ assets/js/buttom-animation.js
@@ -0,0 +1,29 @@
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
+$( "#logo" ).bind({
+  click: function() {
+        $(this).removeClass();
+        $(this).animateCss('bounceOut');
+        setTimeout(function(){ $('#logo').remove(); }, 700);
+        $('#slogan').removeClass('animated flipInY');
+        $('#slogan').addClass('animated fadeOut');
+        setTimeout(function(){ $('.anime').stop().addClass('active'); }, 800);
+        setTimeout(function(){ $('#formulario').stop().addClass('active animated flash'); }, 1500);
+        setTimeout(function(){ $('#logo2').stop().addClass('active animated FadeInUp'); }, 1500);
+        
+  },
+  mouseenter: function() {
+        $(this).removeClass();
+        $(this).animateCss('rubberBand');
+  },
+  mouseleave: function() {
+        $(this).animateCss('rubberBand');
+  }
+});
