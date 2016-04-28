7c3ae063109377e7a3feb3cfa3e07e5cc9d43aba
Marcos Flores <sherix88@gmail.com>
2016-04-28 14:22:26 -0300

    quiene somos y contacto

--- assets/js/buttom-animation.js
+++ assets/js/buttom-animation.js
@@ -17,6 +17,8 @@ $( "#logo" ).bind({
         setTimeout(function(){ $('.anime').stop().addClass('active'); }, 800);
         setTimeout(function(){ $('#formulario').stop().addClass('active animated FadeIn'); }, 1500);
         setTimeout(function(){ $('#logo2').stop().addClass('active animated FadeInUp'); }, 1500);
+        $('.boton_quienes_somos').addClass('animated fadeOut');
+        $('.boton_consulta').addClass('animated fadeOut');
         
   },
   mouseenter: function() {
@@ -26,4 +28,36 @@ $( "#logo" ).bind({
   mouseleave: function() {
         $(this).animateCss('rubberBand');
   }
+});
+
+$( ".boton_quienes_somos" ).bind({
+  click: function() {
+            $('#logo').removeClass();
+        $('#logo').animateCss('bounceOut');
+        setTimeout(function(){ $('#logo').remove(); }, 700);
+        $('#slogan').removeClass('animated flipInY');
+        $('#slogan').addClass('animated fadeOut');
+        setTimeout(function(){ $('.anime').stop().addClass('active'); }, 800);
+        setTimeout(function(){ $('#quienes_somos').stop().addClass('active animated FadeIn'); }, 1500);
+        setTimeout(function(){ $('#logo2').stop().addClass('active animated FadeInUp'); }, 1500);
+        $('.boton_quienes_somos').addClass('animated fadeOut');
+        $('.boton_consulta').addClass('animated fadeOut');
+        
+  },
+});
+
+$( ".boton_consulta" ).bind({
+  click: function() {
+                $('#logo').removeClass();
+        $('#logo').animateCss('bounceOut');
+        setTimeout(function(){ $('#logo').remove(); }, 700);
+        $('#slogan').removeClass('animated flipInY');
+        $('#slogan').addClass('animated fadeOut');
+        setTimeout(function(){ $('.anime').stop().addClass('active'); }, 800);
+        setTimeout(function(){ $('#formulario').stop().addClass('active animated FadeIn'); }, 1500);
+        setTimeout(function(){ $('#logo2').stop().addClass('active animated FadeInUp'); }, 1500);
+        $('.boton_quienes_somos').addClass('animated fadeOut');
+        $('.boton_consulta').addClass('animated fadeOut');
+        
+  },
 });
