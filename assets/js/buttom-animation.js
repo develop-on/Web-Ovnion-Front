2dfaa3ac8e7cb2588276783c34322a366db9b03e
Marcos Flores <sherix88@gmail.com>
2016-06-13 12:11:16 -0300

    google

--- assets/js/buttom-animation.js
+++ assets/js/buttom-animation.js
@@ -6,20 +6,52 @@ $.fn.extend({
         });
     }
 });
-
+//******* Funciones para abrir y cerrar dialogos ******************************************* //
 function  open(dialog) {
+ //remove class **************************************
         $('#logo').removeClass();
-        $('#logo').animateCss('bounceOut');
-        setTimeout(function(){ $('#logo').remove(); }, 700);
         $('#slogan').removeClass('animated flipInY');
+        $('#logo2').removeClass('active animated FadeOut');
+//****************************************************
+        $('#logo').animateCss('bounceOut');
+        setTimeout(function(){ $('#logo').hide(); }, 700);
         $('#slogan').addClass('animated fadeOut');
         setTimeout(function(){ $('.anime').stop().addClass('active'); }, 800);
         setTimeout(function(){ $(dialog).stop().addClass('active animated FadeIn'); }, 1500);
         setTimeout(function(){ $('#logo2').stop().addClass('active animated FadeInUp'); }, 1500);
-        $('.boton_quienes_somos').addClass('animated fadeOut');
-        $('.boton_consulta').addClass('animated fadeOut');
+        setTimeout(function(){ $('.boton_quienes_somos, .boton_consulta').addClass('animated FadeOut'); }, 100);
 }
 
+function  close(dialog) {
+ //remove class **************************************
+        $('.anime').removeClass('active');
+        setTimeout(function(){ $('.boton_quienes_somos, .boton_consulta').removeClass('animated FadeOut'); }, 800);
+        $(dialog).removeClass('active');
+//****************************************************
+        $('#logo2').addClass('active animated FadeOut');
+        setTimeout(function(){ $('#logo').show(); }, 800);
+        setTimeout(function(){ $('#logo').stop().addClass('active animated FadeIn'); }, 800);
+        setTimeout(function(){ $('#slogan').stop().addClass('active animated flipInY'); }, 800);
+        setTimeout(function(){ $('.boton_quienes_somos, .boton_consulta').addClass('animated FadeIn'); }, 800);
+}
+//^***************************************************************************************//
+
+//******* Cerrar Dialogo ******* //
+$( ".cerrar_dialogo1" ).bind({
+  click: function() {
+      close('#quienes_somos');       
+  },
+});
+
+$( ".cerrar_dialogo2" ).bind({
+  click: function() {
+      close('#formulario');       
+  },
+});
+//*******************************//
+
+
+//******* Click Logo  ***************** //
 
 $( "#logo" ).bind({
   click: function() {
@@ -33,17 +65,23 @@ $( "#logo" ).bind({
         $(this).animateCss('rubberBand');
   }
 });
+//**************************************//
 
+//******* Click quienes somos  ***************** //
 $( ".boton_quienes_somos" ).bind({
   click: function() {
 open('#quienes_somos');  
         
   },
 });
+//**************************************//
 
+//******* Click consulta  ***************** //
 $( ".boton_consulta" ).bind({
   click: function() {
 open('#formulario');  
         
   },
 });
+
+//**************************************//
